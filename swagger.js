8bf9ca16a694fb0af1ab6ee1bc4e9967f08d3d64
Stacy Yarrington <syarrington@enuit.com>
2025-09-27 11:39:23 -0600

    week 3 cors

--- swagger.js
+++ swagger.js
@@ -2,11 +2,9 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Users Api',
-    description: 'Users Api',
+    title: 'Recipes Api',
+    description: 'Recipes Api',
   },
-  host: 'localhost:3000',
-  schemes: ['https', 'http'],
 };
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
